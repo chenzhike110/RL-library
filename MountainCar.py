2d6c543f8636694f6f966b2ca582022e84e22e06
chenzhike110 <3180103610@zju.edu.cn>
2021-02-28 13:49:02 +0800

    add MonteCarlo method

---
+++ MountainCar.py
@@ -0,0 +1,21 @@
+import gym
+from gym import spaces
+import time
+env = gym.make('MountainCar-v0')
+env = env.unwrapped
+
+print(env.action_space)
+print(env.observation_space)
+print(env.observation_space.high)
+print(env.observation_space.low)
+
+# env.reset()
+
+# for i in range(10000):
+#     action = env.action_space.sample()
+#     observation, reward, done, info = env.step(action)
+#     if done:
+#         print("over!")
+#         break
+#     env.render()
+#     time.sleep(0.1)
