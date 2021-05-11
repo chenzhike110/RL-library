4c1f56ab67e15f3b97a80e37b73272069ef4a9e3
chenzhike110 <3180103610@zju.edu.cn>
2021-05-11 15:17:31 +0800

    A3C

--- MountainCar.py
+++ MountainCar.py
@@ -5,6 +5,7 @@ env = gym.make('MountainCar-v0')
 env = env.unwrapped
 
 print(env.action_space)
+print(env.action_space.sample())
 print(env.observation_space)
 print(env.observation_space.high)
 print(env.observation_space.low)
